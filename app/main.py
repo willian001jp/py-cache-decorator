b68fa6dc90999a998f28f8efb58a6c4b91c28a13
Willian Nomiyama <willian001@gmail.com>
2025-03-26 17:25:44 -0300

    Solution

--- app/main.py
+++ app/main.py
@@ -2,5 +2,22 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    # Dictionary to store cached results for each function
+    func_cache = {}
+
+    def wrapper(*args, **kwargs) -> str:
+        # Create a key from the function name and arguments
+        # Since we're only dealing with immutable arguments,
+        # we can use them as dict keys
+        key = (func.__name__, args, frozenset(kwargs.items()))
+
+        if key in func_cache:
+            print("Getting from cache")
+            return func_cache[key]
+        else:
+            print("Calculating new result")
+            result = func(*args, **kwargs)
+            func_cache[key] = result
+            return result
+
+    return wrapper
